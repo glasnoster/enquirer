9fb28a2e2e63c02b0ec95d78e011113e439d8094
Martin Pretorius <glasnoster@gmail.com>
2016-07-23 13:44:41 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,16 +2,28 @@ defmodule Enquirer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :enquirer,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :enquirer,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      description: "Enquirer is a simple module to make is easy to get user input in terminal applications.",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package()
+    ]
   end
 
   def application do
     [applications: [:logger]]
+  end
+
+  defp package do
+    [
+      maintainers: ["Martin Pretorius"],
+      licenses:    ["MIT"],
+      links:       %{"GitHub" => "https://github.com/glasnoster/enquirer"}
+    ]
   end
 
   defp deps do
